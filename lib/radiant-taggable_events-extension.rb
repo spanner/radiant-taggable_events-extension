6173eabb8ff527e38b718104eec3b9fa4de544c0
William Ross <will@spanner.org>
2011-10-05 15:52:55 +0100

    version bump

--- lib/radiant-taggable_events-extension.rb
+++ lib/radiant-taggable_events-extension.rb
@@ -1,5 +1,5 @@
 module RadiantTaggableEventsExtension
-  VERSION = '2.0.0'
+  VERSION = '2.0.1'
   SUMMARY = %q{Adds event tags, tag clouds and faceted search to the event_calendar extension.}
   DESCRIPTION = %q{A tiny bit of glue to attach tags to event_calendar events and put tag clouds and faceted retrieval on event calendar pages.}
   URL = "http://spanner.org/radiant/taggable_event"
